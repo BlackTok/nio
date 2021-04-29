f4839fc47eba0b20425ab93cf4f302af825835de
BlackTok <nu.pomytkin@gmail.com>
2021-04-29 16:31:16 +0500

    init

---
+++ server/src/main/java/nio/SystemCommand.java
@@ -0,0 +1,12 @@
+package nio;
+
+public class SystemCommand {
+    protected static final String COMMAND_LS = "ls";
+    protected static final String COMMAND_CD = "cd";
+    protected static final String COMMAND_MKDIR = "mkdir";
+    protected static final String COMMAND_TOUCH = "touch";
+
+    protected static final String SYS_MESSAGE_FILE_EXISTS = "Файл с таким именем уже существует.";
+    protected static final String SYS_MESSAGE_DIR_NOT_EXISTS = "Неверный путь.";
+    //private static final String
+}
