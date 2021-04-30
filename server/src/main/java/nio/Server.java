743d45c6d24e618b7061e28d27aea925258d1772
BlackTok <nu.pomytkin@gmail.com>
2021-04-30 08:59:02 +0500

    LS_MKDIR_TOUCH

--- server/src/main/java/nio/Server.java
+++ server/src/main/java/nio/Server.java
@@ -1,6 +1,6 @@
 package nio;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
@@ -8,8 +8,9 @@ import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -66,7 +67,103 @@ public class Server {
             buffer.clear();
 
             String message = stringBuilder.toString();
+
+            checkMessage(message, key);
         }
+    }
+
+    private void checkMessage(String message, SelectionKey key) throws IOException {
+        String[] msgParts = message.split("\\s");
+        if (msgParts.length == 0)
+            return;
+
+        String command = msgParts[0];
+
+        switch (command) {
+            case SystemCommand.COMMAND_LS:
+                commandLS(msgParts, key);
+                break;
+            case SystemCommand.COMMAND_TOUCH:
+                commandTouch(msgParts, key);
+                break;
+            case SystemCommand.COMMAND_MKDIR:
+                commandMkDir(msgParts, key);
+                break;
+            case SystemCommand.COMMAND_CD:
+                commandCD(msgParts);
+                break;
+        }
+    }
+
+    private void sendMessage(String message, SelectionKey key) throws IOException {
+        SocketChannel channel = (SocketChannel) key.channel();
+        channel.write(ByteBuffer.wrap(message.getBytes(StandardCharsets.UTF_8)));
+    }
+
+    private void commandLS(String[] parts, SelectionKey key) throws IOException {
+        String strPath = "";
+        for (int i = 1; i < parts.length; i++) {
+            strPath = strPath + parts[i];
+        }
+        Path path = Paths.get(strPath);
+
+        if (!Files.exists(path) || !Files.isDirectory(path)) {
+            sendMessage(SystemCommand.SYS_MESSAGE_DIR_NOT_EXISTS, key);
+            return;
+        }
+
+        Files.walkFileTree(path, new HashSet<>(), 1, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                sendMessage(file.toString() + System.lineSeparator(), key);
+                return super.visitFile(file, attrs);
+            }
+        });
+    }
+
+    private void commandTouch(String[] parts, SelectionKey key) throws IOException {
+        String strPath = "";
+        for (int i = 1; i < parts.length; i++) {
+            strPath = strPath + parts[i];
+        }
+        Path path = Paths.get(strPath);
+
+        if (!Files.exists(path) || Files.isDirectory(path)) {
+            sendMessage(SystemCommand.SYS_MESSAGE_DIR_NOT_EXISTS, key);
+            return;
+        }
+
+        InputStream is = new FileInputStream(strPath);
+        byte[] buffer = new byte[256];
+        int read;
+
+        while (true) {
+            read = is.read(buffer);
+            if (read == -1) {
+                break;
+            }
+
+            sendMessage(new String(buffer, StandardCharsets.UTF_8) + System.lineSeparator(), key);
+        }
+    }
+
+    private void commandMkDir(String[] parts, SelectionKey key) throws IOException {
+        String strPath = "";
+        for (int i = 1; i < parts.length; i++) {
+            strPath = strPath + parts[i];
+        }
+        Path path = Paths.get(strPath);
+
+        if (Files.exists(path) && Files.isDirectory(path)) {
+            sendMessage(SystemCommand.SYS_MESSAGE_FILE_EXISTS, key);
+            return;
+        }
+
+        Files.createDirectory(path);
+    }
+
+    private void commandCD(String[] parts) {
+
     }
 
     private void handleAccept(SelectionKey key) throws IOException {
--- server/src/main/java/nio/SystemCommand.java
+++ server/src/main/java/nio/SystemCommand.java
@@ -6,7 +6,7 @@ public class SystemCommand {
     protected static final String COMMAND_MKDIR = "mkdir";
     protected static final String COMMAND_TOUCH = "touch";
 
-    protected static final String SYS_MESSAGE_FILE_EXISTS = "Файл с таким именем уже существует.";
-    protected static final String SYS_MESSAGE_DIR_NOT_EXISTS = "Неверный путь.";
+    protected static final String SYS_MESSAGE_FILE_EXISTS = "Файл с таким именем уже существует." + System.lineSeparator();
+    protected static final String SYS_MESSAGE_DIR_NOT_EXISTS = "Неверный путь." + System.lineSeparator();
     //private static final String
 }
